a3172994e1e47df3ccfe4edfdf22625256e4568c
adrianAlday <adrian.alday@leafly.com>
2020-11-13 12:13:54 -0500

    reorder

--- pages/index.js
+++ pages/index.js
@@ -89,6 +89,11 @@ const Home = ({ devices }) => {
   return (
     <div className="container">
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no"
+        />
+
         <title>vibe check 🌵⚡🌷</title>
 
         <link rel="icon" href="/favicon.ico" />
@@ -96,11 +101,6 @@ const Home = ({ devices }) => {
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans"
-        />
-
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no"
         />
       </Head>
 
